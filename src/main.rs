4b89304315c3c46d5af4816e8ef79462c7bc3cdd
Loïc Grobol <loic.grobol@gmail.com>
2019-03-19 14:11:48 +0100

    initial

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use memchr;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: rustine <file>");
+        std::process::exit(1);
+    }
+    let in_file = &args[1];
+
+    let mut f = std::io::BufReader::new(std::fs::File::open(in_file).expect("open failed"));
+    
+    eprintln!("{}", count_char(&mut f, b'\n').unwrap());
+
+}
+
+fn count_char<R: std::io::BufRead + ?Sized>(r: &mut R, delim: u8)-> Result<usize, std::io::Error> {
+    // Empty file
+    if r.fill_buf().unwrap().len() == 0 {
+        return Ok(0);
+    };
+    let mut count = 1;
+    loop {
+        let (found, used) = {
+            let available = match r.fill_buf() {
+                Ok(n) => n,
+                Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => continue,
+                Err(e) => return Err(e)
+            };
+            match memchr::memchr(delim, available) {
+                Some(i) => {
+                    (true, i + 1)
+                }
+                None => {
+                    (false, available.len())
+                }
+            }
+        };
+        if found {
+            count += 1;
+        }
+        r.consume(used);
+        if used == 0 {
+            return Ok(count);
+        }
+    }
+}
