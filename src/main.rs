72c2bf331ff4fbe4c4e04c1a11afb446fe614686
Loïc Grobol <loic.grobol@gmail.com>
2019-03-19 15:53:39 +0100

    :art:

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,22 @@ fn main() {
     let in_file = &args[1];
 
     let mut f = std::io::BufReader::new(std::fs::File::open(in_file).expect("open failed"));
-    
-    eprintln!("{}", count_char(&mut f, b'\n').unwrap());
 
+    eprintln!("{}", count_char(&mut f, b'\n').unwrap());
 }
 
-fn count_char<R: std::io::BufRead + ?Sized>(r: &mut R, delim: u8)-> Result<usize, std::io::Error> {
-    let mut count =0;
+fn count_char<R: std::io::BufRead + ?Sized>(r: &mut R, delim: u8) -> Result<usize, std::io::Error> {
+    let mut count = 0;
     loop {
         let (found, used) = {
             let available = match r.fill_buf() {
                 Ok(n) => n,
                 Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => continue,
-                Err(e) => return Err(e)
+                Err(e) => return Err(e),
             };
             match memchr::memchr(delim, available) {
-                Some(i) => {
-                    (true, i + 1)
-                }
-                None => {
-                    (false, available.len())
-                }
+                Some(i) => (true, i + 1),
+                None => (false, available.len()),
             }
         };
         if found {
